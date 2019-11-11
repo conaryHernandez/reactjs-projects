83c1fdf7b6be498a27ef1a5e705395294730954e
Conary <31629328+conaryHernandez@users.noreply.github.com>
2019-11-10 19:04:23 -0600

    navigation components

--- react-apps/burger-projectv1/src/components/Layout/Layout.js
+++ react-apps/burger-projectv1/src/components/Layout/Layout.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import Aux from '../../hoc/Auxiliar';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/ToolBar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = ( props ) => (
     <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <SideDrawer />
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
