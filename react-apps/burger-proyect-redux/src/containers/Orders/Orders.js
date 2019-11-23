f31767e5473558f21fecfe5112cf015023883d97
Conary <31629328+conaryHernandez@users.noreply.github.com>
2019-11-23 06:47:55 -0600

    orders with redux

--- react-apps/burger-proyect-redux/src/containers/Orders/Orders.js
+++ react-apps/burger-proyect-redux/src/containers/Orders/Orders.js
@@ -1,38 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../store/actions/index';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
-	state = {
-		orders: [],
-		loading: true,
-	}
 
 	componentDidMount() {
-		axios.get('/orders.json')
-			.then(res => {
-				const fetchedOrders = [];
-
-				for (let key in res.data) {
-					fetchedOrders.push({
-						...res.data[key],
-						id: key
-					});
-				}
-				this.setState({ orders: fetchedOrders, loading: false });
-			})
-			.catch(error => {
-				this.setState({ loading: false });
-				console.log('error', error);
-			});
+		this.props.onInitOrders();
 	}
 
 	buildOrders = () => {
 		let orders = null;
 
-		orders = this.state.orders.map(order => {
+		orders = this.props.orders.map(order => {
 			return <Order key={order.id} ingredients={order.ingredients} price={order.price} />
 		});
 
@@ -40,6 +24,10 @@ class Orders extends Component {
 	}
 
 	render() {
+		if (this.props.loading) {
+			return <Spinner />;
+		}
+
 		return (
 			<div>
 				{this.buildOrders()}
@@ -48,4 +36,18 @@ class Orders extends Component {
 	}
 }
 
-export default withErrorHandler(Orders, axios);
+const mapStateToProps = state => {
+	return {
+		orders: state.order.orders,
+		loading: state.order.loading
+	};
+};
+
+const mapDispatchToProps = dispatch => {
+	return {
+		onInitOrders: () => dispatch(actions.fetchOrders())
+	};
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
