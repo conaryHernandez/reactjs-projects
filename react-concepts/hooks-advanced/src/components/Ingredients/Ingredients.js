80a248d0dfebc4ba15ad45d540bac2b3211809cc
Conary <31629328+conaryHernandez@users.noreply.github.com>
2019-11-27 14:36:18 -0600

    using useEffect hook

--- react-concepts/hooks-advanced/src/components/Ingredients/Ingredients.js
+++ react-concepts/hooks-advanced/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import IngredientForm from './IngredientForm';
@@ -8,14 +8,47 @@ import Search from './Search';
 
 function Ingredients() {
   const [ ingredients, setIngredients ] = useState([]);
+  const [ filteredIngredients, setFilteredIngredients ] = useState([]);
+  const [ ingredientsToDisplay, setIngredientsToDisplay ] = useState(ingredients);
+
+  useEffect(() => {
+
+    async function fetchIngredients() {
+      
+      try {
+        const response = await axios.get('https://food-22d6b.firebaseio.com/ingredients.json');
+        const loadedIngredients = [];
+
+        for(const key in response.data) {
+          loadedIngredients.push({
+            id:  key,
+            title: response.data[key].title,
+            amount: response.data[key].amount,
+          });
+        }
+
+        setIngredients(loadedIngredients);
+        
+      } catch (error) {
+        console.log(error);
+      }  
+    }
+    fetchIngredients();
+  }, []);
+
+  useEffect(() => {
+    if (filteredIngredients.length > 0) {
+      setIngredientsToDisplay(filteredIngredients);
+    } else {
+      setIngredientsToDisplay(ingredients);
+    }
+  }, [filteredIngredients, ingredients]);
 
   const addIngredientHandler = async (ingredient) => {
     const response = await axios.post('https://food-22d6b.firebaseio.com/ingredients.json', ingredient);
 
-    console.log(response);
-
     setIngredients(prevState => [...prevState, {
-      id: Math.random().toString(),
+      id: response.name,
       ...ingredient
     }]);
   }
@@ -24,12 +57,16 @@ function Ingredients() {
     setIngredients(prevIngredients => prevIngredients.filter(ig => ig.id !== id));
   };
 
+  const filterIngredientsHandler = value => {
+    setFilteredIngredients(ingredients.filter(ig => ig.title.includes(value)))
+  };
+
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />  
       <section>
-        <Search />
-        <IngredientList ingredients={ingredients} onRemoveItem={removeIngredientHandler} />
+        <Search onSearch={filterIngredientsHandler}/>
+        <IngredientList ingredients={ingredientsToDisplay} onRemoveItem={removeIngredientHandler} />
      </section>
     </div>
   );
--- react-concepts/hooks-advanced/src/components/Ingredients/Search.js
+++ react-concepts/hooks-advanced/src/components/Ingredients/Search.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const [ enteredFilter, setEnteredFilter ] = useState('');
+  const { onSearch } = props;
+
+  const onSerachHandler = (e) => {
+    setEnteredFilter(e.target.value);
+    onSearch(e.target.value);
+  }
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input type="text" value={enteredFilter} onChange={onSerachHandler} />
         </div>
       </Card>
     </section>
